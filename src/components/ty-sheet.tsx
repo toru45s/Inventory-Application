4b6cac0af4ecd204546f15c1dd1ec0972fe0e57a
t-yaginuma <toru45s@gmail.com>
2025-03-20 16:14:21 -0700

    update sheet component

--- src/components/ty-sheet.tsx
+++ src/components/ty-sheet.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import {
   Sheet,
   SheetContent,
@@ -10,19 +11,47 @@ import {
 type Props = {
   title: string;
   description: string;
+  price: number;
+  category: string;
+  stock: number;
+  imageUrl: string;
+  sku: string;
+  rating: {
+    rate: number;
+    count: number;
+  };
   children: React.ReactNode;
 };
 
 export function TySheet(props: Props) {
-  const { title, description, children } = props;
+  const {
+    title,
+    description,
+    price,
+    category,
+    stock,
+    sku,
+    imageUrl,
+    rating,
+    children,
+  } = props;
 
   return (
     <Sheet>
-      <SheetTrigger>{children}</SheetTrigger>
+      <SheetTrigger asChild>{children}</SheetTrigger>
       <SheetContent>
         <SheetHeader>
           <SheetTitle>{title}</SheetTitle>
+          <Image src={imageUrl} width={350} height={350} alt={title} />
           <SheetDescription>{description}</SheetDescription>
+          <SheetDescription>CAD: {price}</SheetDescription>
+          <SheetDescription>Category: {category}</SheetDescription>
+          <SheetDescription>Number of stocks: {stock}</SheetDescription>
+
+          <SheetDescription>
+            Rate: {rating.rate} ({rating.count})
+          </SheetDescription>
+          <SheetDescription>SKU: {sku}</SheetDescription>
         </SheetHeader>
       </SheetContent>
     </Sheet>
