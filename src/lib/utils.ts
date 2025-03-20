19b44677f9e3c8a65091f480f24d5c46e1e81d68
t-yaginuma <toru45s@gmail.com>
2025-03-20 16:12:21 -0700

    add convertDate Function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,17 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import dayjs from "dayjs";
+import timezone from "dayjs/plugin/timezone";
+import utc from "dayjs/plugin/utc";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs().format();
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const convertDate = (date: string) => {
+  return dayjs(date).tz("America/Vancouver").format("MMMM DD YYYY");
+};
